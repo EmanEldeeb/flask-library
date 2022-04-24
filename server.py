25e907b372e8f8767caf81136cfb395d56dc46b0
eman eldeeb <emaneldeeb1999@gmail.com>
2022-04-24 17:49:56 +0200

    add edit route

--- server.py
+++ server.py
@@ -22,6 +22,7 @@ def index():
 
 @app.route('/add',methods = ['POST'])
 def add():
+    con = sqlite3.connect(f'{DBNAME}.db')
     try:
         title = request.form['title']
         category = request.form['category']
@@ -31,11 +32,33 @@ def add():
         description = request.form['description']
         pagenumber = request.form['pagenumber']
         
-        with sqlite3.connect(f'{DBNAME}.db') as con:
-            cur = con.cursor()
-            cur.execute(f'INSERT INTO {BOOK_T} (BookTitle,BookCategory,BookIsbn,BookLanguage,BookEdition,BookDescription,BookPagesNo) VALUES (?,?,?,?,?,?,?)', (title,category,isbn,language,edition,description,pagenumber))
-            con.commit()
-            print("added new row")
+        cur = con.cursor()
+        cur.execute(f'INSERT INTO {BOOK_T} (BookTitle,BookCategory,BookIsbn,BookLanguage,BookEdition,BookDescription,BookPagesNo) VALUES (?,?,?,?,?,?,?)', (title,category,isbn,language,edition,description,pagenumber))
+        con.commit()
+        print("added new row")
+    except Exception as err:
+        con.rollback()
+        print(f'SQLite error: {err}')
+    finally:
+        con.close()
+        return redirect(url_for("index"))
+
+@app.route('/edit', methods = ['POST'])
+def edit():
+    con = sqlite3.connect(f'{DBNAME}.db')
+    try:
+        title = request.form['title']
+        category = request.form['category']
+        isbn = request.form['isbn']
+        language = request.form['language']
+        edition = request.form['edition']
+        description = request.form['description']
+        pagenumber = request.form['pagenumber']
+        
+        cur = con.cursor()
+        cur.execute(f'UPDATE {BOOK_T} SET BookTitle=?,BookCategory=?,BookIsbn=?,BookLanguage=?,BookEdition=?,BookDescription=?,BookPagesNo=? WHERE BookTitle=?', (title,category,isbn,language,edition,description,pagenumber,title))
+        con.commit()
+        print("updated new row")
     except Exception as err:
         con.rollback()
         print(f'SQLite error: {err}')
